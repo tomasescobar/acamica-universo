d0708d18da1acc8e0b6e233d701323f4a5107b43
flycode <nicobrizuela@gmail.com>
2014-03-07 15:35:21 -0300

    Encuestas: add folder

---
+++ encuestas/js/main.js
@@ -0,0 +1,40 @@
+$(document).ready(function() {
+      
+      // Menu Box Toggle
+      var $toggleMenu = $('.toggle-menu'),
+          $menuBox = $('.menu-box'),
+          $iframebox = $('.iframebox'),
+          $iframe = $iframebox.children('iframe');
+
+      
+      // Change iframe
+      var $fullBtn = $('.full-btn'),
+          $mediumBtn = $('.medium-btn'),
+          $smallBtn = $('.small-btn');
+      
+      $fullBtn.click(function() {
+         $iframebox.attr('id','fullframe');
+      });
+      $mediumBtn.click(function() {
+         $iframebox.attr('id','mediumframe');
+      });
+      $smallBtn.click(function() {
+         $iframebox.attr('id','smallframe');
+      });
+
+      //
+      $(".change-layout a").on("click", function(e){
+         e.preventDefault();
+         console.log($(this).attr('href'))
+         $iframe.attr('src',$(this).attr('href'));
+         $(".change-layout a").removeClass('selected');
+         $(this).addClass('selected');
+      });
+
+      // Set height
+      $iframe.height(1500)
+      // $('iframe').load(function() {
+      //   this.style.height = this.contentWindow.document.body.offsetHeight + 'px';
+      // });
+
+});
